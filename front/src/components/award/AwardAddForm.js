51ada41c53383931894e135a1b3b9bd3907ef11d
jinah park <tbrk8585@naver.com>
2022-03-17 01:12:28 +0900

    [Update] award-test 최종본

--- front/src/components/award/AwardAddForm.js
+++ front/src/components/award/AwardAddForm.js
@@ -1,37 +1,46 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
 import {Form, Button, Row, Col} from 'react-bootstrap';
 import * as Api from "../../api";
+import { UserStateContext } from "../../App";
 
 // 수상이력 추가 컴포넌트로 {폼 활성화 여부 state}, {user.id}, {리스트 업데이트 함수}를 props로 받아옵니다. 
-function AwardAddForm ({setAddAward, portfolioOwnerId, setAwardList}) {
+function AwardAddForm ({setAddAward, setAwardLists}) {
     // 수상내역, 상세내역 state 
-    const [awardtitle, setAwardtitle] = useState('');
-    const [awardDtail, setAwardDtail] = useState('');
-    // **확인 버튼 시, 보내질 form data 형식&내용 ** 확인버튼 구현 예정 (submit)
-    async function addSubmitHandler (e) {
-        e.preventDefault();
-        const uptAwardData = {
-            userId: portfolioOwnerId,
-            title: awardtitle,
-            description: awardDtail,
-        }
-        
-        // ** 데이터를 보내고, ** 수정 ** 보낸 데이터를 기존 리스트에 추가 후, setAwardList에 업데이트 해야함 
-        await Api.post('award/create', uptAwardData)
-    
-        const updateList = await Api.get('awardlist', portfolioOwnerId )
-        setAwardList(updateList);
-        setAddAward(false)
-    }
+    const userState = useContext(UserStateContext);
+    const user_id = userState.user.id;
 
+    const [awardTitle, setAwardTitle] = useState('');
+    const [awardDetail, setAwardDetail] = useState('');
+    // **확인 버튼 시, 보내질 form data 형식&내용 ** 확인버튼 구현 예정 (submit)
+
+    const addSubmitHandler = async (e) => {
+        e.preventDefault();
+    
+  
+        const uptAwardData = {
+          user_id,
+          title: awardTitle,
+          description: awardDetail,
+        };
+    
+        await Api.post("award/create", uptAwardData);
+    
+        
+        const updateList = await Api.get("awardlist", user_id);
+        setAwardLists(updateList.data);
+    
+        setAddAward(false);
+      };
+    
+    
     return(
-        <Form onSubmit={(e)=> addSubmitHandler(e)}>
+        <Form onSubmit={addSubmitHandler}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Control type="text" placeholder="수상내역" value={awardtitle} onChange={(e)=>setAwardtitle(e.target.value)}/>
+                <Form.Control type="text" placeholder="수상내역" value={awardTitle} onChange={(e)=>setAwardTitle(e.target.value)}/>
             </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Control type="text" placeholder="상세내역" value={awardDtail} onChange={(e)=>setAwardDtail(e.target.value)}/>
+            <Form.Control type="text" placeholder="상세내역" value={awardDetail} onChange={(e)=>setAwardDetail(e.target.value)}/>
         </Form.Group>
         <Row className="text-center">
             <Col>
--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {Card, Row, Col, Button} from "react-bootstrap";
 import AwardAddForm from "./AwardAddForm";
 import Award from './Award';
 import * as Api from "../../api";
+import { UserStateContext } from "../../App";
 
 // 메인 컴포넌트라고 할 수 있습니다. 
 // portfolioOwnerId = user.id 로, 유저별 데이터를 불러오거나 작성시 필요 
@@ -10,20 +11,17 @@ function AwardCard ({portfolioOwnerId, isEditable}) {
     // award 추가할 폼을 열고 닫는 토글 기능 
     const [addAward, setAddAward] = useState(false);
     // 추가한 리스트들이 잘 뿌려지는지 가짜 데이터 (추후 삭제 예정)
+    const userState = useContext(UserStateContext);
 
+    const user_id = userState.user.id;
 
-    const fake = [
-      {title: '행복상', description: '행복해서 받았습니다.'},
-      {title: '궁금상', description: '매 사 잘 할 수 있을까 궁금합니다.'},
-      {title: '개발상', description: '미래 개발자로 거듭나겠습니다.'}
-  ]
    // api가 완성된다면, 가짜데이터가 아닌, 불러온 데이터로 수상이력목록을 업데이트 할 예정
    // 이 때, user.id가 바뀌면 수상리스트를 다시 불러옴 
-    const [awardList, setAwardList] = useState(fake)
+    const [awardLists, setAwardLists] = useState([])
 
-    // useEffect(() =>
-    //     Api.get('awardlist', portfolioOwnerId).then((res) => setAwardList(res.data))
-    //,[portfolioOwnerId])
+    useEffect(() =>
+        Api.get("awardlist", portfolioOwnerId).then((res) => setAwardLists(res.data))
+    ,[portfolioOwnerId])
 
     return(
     <>
@@ -33,8 +31,8 @@ function AwardCard ({portfolioOwnerId, isEditable}) {
             <Card.Title>수상이력</Card.Title>
           </Row>
           <Row>
-            {awardList.map((award) =>
-              <Award portfolioOwnerId={portfolioOwnerId} award={award} isEditable={isEditable} setAwardList={setAwardList}/>
+            {awardLists.map((award) =>
+              <Award award={award} isEditable={isEditable} setAwardLists={setAwardLists}/>
             )}
           </Row>
           {isEditable&&(
@@ -51,7 +49,7 @@ function AwardCard ({portfolioOwnerId, isEditable}) {
           
            {addAward&&(
             <Row>
-                <AwardAddForm setAddAward={setAddAward} portfolioOwnerId={portfolioOwnerId} setAwardList={setAwardList}/>
+                <AwardAddForm setAddAward={setAddAward} setAwardLists={setAwardLists}/>
             </Row>
            )}
         </Card.Body>
