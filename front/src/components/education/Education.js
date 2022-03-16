85ba127560c4f56bf71937a2e5ce9f8773a5aae8
rnrn99 <giu0213@naver.com>
2022-03-16 23:44:09 +0900

    delete-test

--- front/src/components/education/Education.js
+++ front/src/components/education/Education.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import EducationCard from "./EducationCard";
 import EducationEditForm from "./EducationEditForm";
-
+// 가짜 주석 추가
 function Education({ education, setEducations, isEditable }) {
   const [clickEditBtn, setClickEditBtn] = useState(false); // 편집 버튼 클릭 상태를 저장합니다.
   return (
