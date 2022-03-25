77f7b79d6f438ff16fcbbd2de302ad444f05516d
Shin <tlsrhkdcjs12@naver.com>
2022-03-25 22:23:51 +0900

    test

--- front/src/index.js
+++ front/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import dotenv from "dotenv/config";
+require("dotenv").config();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root"),
+  document.getElementById("root")
 );
